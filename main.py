54c6acdde4894fd5b4544cb85cea6767abd0350e
RoNSipinoa <77037708+RoNSipinoa@users.noreply.github.com>
2022-06-15 15:08:42 +0900

    new

--- main.py
+++ main.py
@@ -1,45 +1,10 @@
 def on_button_pressed_a():
-    radio.send_string(rsp_mine)
+    if randint(1, 10) <= 5:
+        for index in range(5):
+            for index2 in range(5):
+                led.plot(index, index2)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-# rsp
-# 
-# 0 = rock
-# 
-# 1 = scissor
-# 
-# 2 = paper
-
-def on_gesture_shake():
-    global rsp_mine
-    rsp_mine = ["R", "S", "P"][randint(0, 2)]
-    basic.show_string(rsp_mine)
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
-
-def on_received_string(receivedString):
-    radio.send_string(rsp_mine)
-    if rsp_mine == "R":
-        if receivedString == "R":
-            basic.show_string("D")
-        elif receivedString == "S":
-            basic.show_string("W")
-        else:
-            basic.show_string("L")
-    elif rsp_mine == "S":
-        if receivedString == "R":
-            basic.show_string("L")
-        elif receivedString == "S":
-            basic.show_string("D")
-        else:
-            basic.show_string("W")
-    elif receivedString == "R":
-        basic.show_string("W")
-    elif receivedString == "S":
-        basic.show_string("L")
-    else:
-        basic.show_string("D")
-radio.on_received_string(on_received_string)
-
-rsp_mine = ""
-rsp = 0
-radio.set_group(7)
+def on_button_pressed_b():
+    pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,42 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendString(rsp_mine)
-})
-// rsp
-// 
-// 0 = rock
-// 
-// 1 = scissor
-// 
-// 2 = paper
-input.onGesture(Gesture.Shake, function () {
-    rsp_mine = ["R", "S", "P"][randint(0, 2)]
-    basic.showString(rsp_mine)
-})
-radio.onReceivedString(function (receivedString) {
-    radio.sendString(rsp_mine)
-    if (rsp_mine == "R") {
-        if (receivedString == "R") {
-            basic.showString("D")
-        } else if (receivedString == "S") {
-            basic.showString("W")
-        } else {
-            basic.showString("L")
+    if (randint(1, 10) <= 5) {
+        for (let index = 0; index <= 4; index++) {
+            for (let index2 = 0; index2 <= 4; index2++) {
+                led.plot(index, index2)
+            }
         }
-    } else if (rsp_mine == "S") {
-        if (receivedString == "R") {
-            basic.showString("L")
-        } else if (receivedString == "S") {
-            basic.showString("D")
-        } else {
-            basic.showString("W")
-        }
-    } else if (receivedString == "R") {
-        basic.showString("W")
-    } else if (receivedString == "S") {
-        basic.showString("L")
-    } else {
-        basic.showString("D")
     }
 })
-let rsp_mine = ""
-radio.setGroup(7)
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index <= 4; index++) {
+        for (let index2 = 0; index2 <= 4; index2++) {
+            led.unplot(index, index2)
+        }
+    }
+})

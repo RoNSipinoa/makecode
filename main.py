33ea1a7300f117f59b8233f2bfec7af696d40c82
RoNSipinoa <77037708+RoNSipinoa@users.noreply.github.com>
2022-06-15 15:50:31 +0900

    new

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-def on_button_pressed_a():
-    if randint(1, 10) <= 5:
-        for index in range(5):
-            for index2 in range(5):
-                led.plot(index, index2)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
-    pass
-input.on_button_pressed(Button.B, on_button_pressed_b)
+range2: List[number] = []
+x = 0
+def randomOff():
+    global x
+    while len(range2) != 0:
+        x = range2.remove_at(randint(0, len(range2)))
+        led.unplot(x % 5, Math.idiv(x, 5))
+        basic.pause(100)
+def oneTimeOn():
+    basic.show_leds("""
+        # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+    """)
+    basic.pause(1000)
+def rangeOn():
+    for i in range(5):
+        for j in range(5):
+            led.plot(j, i)
+def randomOn():
+    global x
+    while len(range2) != 0:
+        x = range2.remove_at(randint(0, len(range2)))
+        led.plot(x % 5, Math.idiv(x, 5))
+        basic.pause(100)
+def rangeOff():
+    for k in range(5):
+        for l in range(5):
+            led.unplot(l, k)
+def rangeReset():
+    global range2
+    range2 = []
+    for index4 in range(25):
+        range2.append(index4)
+def oneTimeOff():
+    led.stop_animation()
+    basic.pause(1000)
--- main.ts
+++ main.ts
@@ -1,16 +1,82 @@
-input.onButtonPressed(Button.A, function () {
-    if (randint(1, 10) <= 5) {
-        for (let index = 0; index <= 4; index++) {
-            for (let index2 = 0; index2 <= 4; index2++) {
-                led.plot(index, index2)
-            }
+function randomOff () {
+    while (range2.length != 0) {
+        x = range2.removeAt(randint(0, range2.length))
+        led.unplot(x % 5, Math.idiv(x, 5))
+        basic.pause(100)
+    }
+}
+function oneTimeOn () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(1000)
+}
+input.onGesture(Gesture.Shake, function () {
+    rangeReset()
+    // 0: oneTime
+    // 1: random
+    // 2: range
+    act = randint(0, 2)
+    if (state == 0) {
+        if (act == 0) {
+            oneTimeOn()
+        } else if (act == 1) {
+            randomOn()
+        } else {
+            rangeOn()
+        }
+    } else {
+        if (act == 0) {
+            oneTimeOff()
+        } else if (act == 1) {
+            randomOff()
+        } else {
+            rangeOff()
         }
     }
+    // 0:off
+    // 1:on
+    state = 1 - state
 })
-input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index <= 4; index++) {
-        for (let index2 = 0; index2 <= 4; index2++) {
-            led.unplot(index, index2)
+function rangeOn () {
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= 4; j++) {
+            led.plot(j, i)
+            basic.pause(100)
         }
     }
-})
+}
+function randomOn () {
+    while (range2.length != 0) {
+        x = range2.removeAt(randint(0, range2.length))
+        led.plot(x % 5, Math.idiv(x, 5))
+        basic.pause(100)
+    }
+}
+function rangeOff () {
+    for (let k = 0; k <= 4; k++) {
+        for (let l = 0; l <= 4; l++) {
+            led.unplot(l, k)
+            basic.pause(100)
+        }
+    }
+}
+function rangeReset () {
+    range2 = []
+    for (let index4 = 0; index4 <= 24; index4++) {
+        range2.push(index4)
+    }
+}
+function oneTimeOff () {
+    led.stopAnimation()
+    basic.pause(1000)
+}
+let act = 0
+let x = 0
+let range2: number[] = []
+let state = 0
+state = 0
